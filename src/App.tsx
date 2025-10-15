502fd20c2ada327a38d90ef9abdd81ba25e782b2
michael phillips <michael.phillips4@yahoo.co.uk>
2025-10-15 18:50:29 +0100

    fix 1

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export function App() {
   return (
     <>
       <Authenticator>
-        {({ signOut, user }) => (
+        {({ signOut}) => (
           <main>
             <div className='list-row'><h1 className='center list-item-grow'>Lists</h1>
             <button onClick={signOut}>Sign out</button></div>
